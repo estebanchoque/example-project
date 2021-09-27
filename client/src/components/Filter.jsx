45155ee23f2b93d428b56424751f7bc1fe7d2a48
Eliza <lizcano.liz14@gmail.com>
2021-09-27 10:37:52 -0500

    delcaracion de amor

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 
 const filter = () => {
     return ( 
-        <div></div>
+        <div>
+            <h1>Para el amor de mi vida</h1>
+            <p>Esteban, eres el mejor hombre que he conocido, te quiero con todo mi corazon y cuento los dias para poder verte</p>
+        </div>
      );
 }
  
