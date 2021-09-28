d84baf54b9ea0d043f68ba8db38877d7388f525e
estebanchoque <choqueestebanjoaquin@gmail.com>
2021-09-28 00:51:55 -0300

    Add some changes on Filter component

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-
 const filter = () => {
     return ( 
         <div>
             <h1>Para el amor de mi vida</h1>
             <p>Esteban, eres el mejor hombre que he conocido, te quiero con todo mi corazon y cuento los dias para poder verte</p>
+            <p>Te adoro, mi amor. Yo también quiero amarte por siempre!</p>
         </div>
      );
 }
