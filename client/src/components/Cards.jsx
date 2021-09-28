a6d5c71b9d9e94b8004923d7fe14c7b564133dad
estebanchoque <choqueestebanjoaquin@gmail.com>
2021-09-27 21:15:27 -0300

    Add button for view details

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Card from "./Card";
 
 function Cards({ posts }) {
+  const onClick = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
       {posts.map((elem) => (
         <div key={elem.id}>
           <h3>{elem.title}</h3>
           <p>{elem.description}</p>
+          <button onClick={onClick}>View details</button>
         </div>
       ))}
     </div>
