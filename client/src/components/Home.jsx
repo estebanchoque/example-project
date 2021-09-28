382b2af6f6d72f347058f26166b1f03fd7b035f2
estebanchoque <choqueestebanjoaquin@gmail.com>
2021-09-28 00:35:03 -0300

    Add redux folders

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Cards from "./Cards";
 
-const Home = () => {};
+const Home = () => {
+  const posts = [{ id: 1, title: "First post", description: "I am a post" }];
+  return (
+    <div>
+      <Cards posts={posts} />
+    </div>
+  );
+};
 
 export default Home;
